5e3bae3fd0bb3190eeff3f9fb98b616519fdb16f
Tom Aratyn <tom@securitycompass.com>
2008-06-01 11:58:09 +0000

    Build script was misconfigured. Didn't include components in xpi.

--- config_build.sh
+++ config_build.sh
@@ -4,6 +4,6 @@ APP_NAME=accessme
 CHROME_PROVIDERS="content locale skin"
 CLEAN_UP=1
 ROOT_FILES="gpl-3.0.txt readme.txt"
-ROOT_DIRS="defaults"
+ROOT_DIRS="defaults components"
 BEFORE_BUILD=
 AFTER_BUILD=
