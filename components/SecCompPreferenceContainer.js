0098b9740e4fc0c3c1e108d33c7a4b7a12bec1ef
Tom Aratyn <tom@securitycompass.com>
2008-06-02 21:43:08 +0000

    Added licenses everywhere.

--- components/SecCompPreferenceContainer.js
+++ components/SecCompPreferenceContainer.js
@@ -1,5 +1,25 @@
-/* Copyright (c) 2006 SecurityCompass
-   See the file LICENSE.txt for licensing information. */
+/*
+Copyright 2007 Security Compass
+
+This file is part of SQL Inject Me.
+
+Access Meis free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Access Meis distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SQL Inject Me.  If not, see <http://www.gnu.org/licenses/>.
+
+If you have any questions regarding Access Meplease contact
+tools@securitycompass.com
+*/
+
 
 /* module PreferenceContainer */
 
