83280945adcff9b9fb66a5d6fb04f0d4d7684362
Tom Aratyn <tom@securitycompass.com>
2008-07-14 22:07:21 +0000

    Got rid of the build scripts that don't work right on windows.

--- config_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Build config for build.sh
-APP_NAME=accessme
-CHROME_PROVIDERS="content locale skin"
-CLEAN_UP=1
-ROOT_FILES="gpl-3.0.txt readme.txt"
-ROOT_DIRS="defaults components"
-BEFORE_BUILD=
-AFTER_BUILD=
